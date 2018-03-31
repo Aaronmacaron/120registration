47302aebc4b3e10546b976e3c12675993127eb75
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-03-31 17:22:28 +0200

    Implement Login Logic

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,5 @@
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.time.LocalDate;
 
 public class Account {
@@ -11,6 +13,14 @@ public class Account {
         this.email = email;
         this.birthday = birthday;
         this.passwordHash = passwordHash;
+    }
+
+    public Reaction authenticate(String password) {
+        if (BCrypt.checkpw(password, this.passwordHash)) {
+            return Reaction.success();
+        }
+
+        return Reaction.failure();
     }
 
     public String getUsername() {
--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,8 +1,15 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Login implements RegistrationController {
     private Registration registration;
+
+    @FXML private TextField username;
+    @FXML private PasswordField password;
+    @FXML private Label errorMessage;
 
     @Override
     public void loaded() {
@@ -10,12 +17,32 @@ public class Login implements RegistrationController {
     }
 
     @FXML
-    public void handleLoginButtonClick(ActionEvent actionEvent) {
+    public void handleRegisterButtonClicked(ActionEvent actionEvent) {
         registration.getPrimaryStage().setScene(registration.getRegisterScene());
+    }
+
+    @FXML
+    public void handleLoginButtonClicked(ActionEvent actionEvent) {
+        authenticateUser()
+                .succeed(reaction -> registration.getPrimaryStage().setScene(registration.getAccountsScene()))
+                .fail(reaction -> errorMessage.setText(reaction.getMessage()));
     }
 
     @Override
     public void setRegistration(Registration registration) {
         this.registration = registration;
+    }
+
+    private Reaction authenticateUser() {
+        boolean authenticated = registration.getAccounts()
+                .stream()
+                .filter(account -> account.getUsername().equals(username.getText()))
+                .anyMatch(account -> account.authenticate(password.getText()).isSuccess());
+
+        if (authenticated) {
+            return Reaction.success();
+        }
+
+        return Reaction.failure("The username or password is not correct.");
     }
 }
