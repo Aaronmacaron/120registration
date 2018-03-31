ed528e911484be92edfaf1949fa12b49fd422646
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-03-31 18:37:29 +0200

    Add additional validation to register form and improve overall validation logic

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -7,6 +7,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 public class Register implements RegistrationController {
 
     @FXML private VBox formContainer;
@@ -26,12 +30,10 @@ public class Register implements RegistrationController {
 
     @FXML
     public void handleSendButtonClick(ActionEvent actionEvent) {
-        validatePassword()
-                .succeed(reaction -> {
-                    registration.getAccounts().add(getAccountFromForm());
-                    registration.getPrimaryStage().setScene(registration.getAccountsScene());
-                })
-                .fail(reaction -> errorMessage.setText(reaction.getMessage()))
+        validate().succeed(reaction -> {
+            registration.getAccounts().add(getAccountFromForm());
+            registration.getPrimaryStage().setScene(registration.getAccountsScene());
+        }).fail(reaction -> errorMessage.setText(reaction.getMessage()))
                 .resolve(reaction -> errorMessage.setText(reaction.getMessage()));
     }
 
@@ -42,6 +44,78 @@ public class Register implements RegistrationController {
 
     public void setRegistration(Registration registration) {
         this.registration = registration;
+    }
+
+    private Reaction validate() {
+        final Stream<Reaction> reactions = Stream.of(
+                validatePassword(),
+                validateUsername(),
+                validateEmail(),
+                validateBirthday()
+        );
+
+        return reactions.filter(Reaction::isFailure)
+                .findFirst()
+                .orElseGet(Reaction::success);
+    }
+
+    private Reaction validateBirthday() {
+        final LocalDate date = birthday.getValue();
+
+        if (date == null) {
+            return Reaction.failure("Please provide a valid birthday.");
+        }
+
+        if (date.isAfter(LocalDate.now())) {
+            return Reaction.failure("Your birthday can't be after after today.");
+        }
+
+        return Reaction.success();
+    }
+
+    private Reaction validateEmail() {
+        final String emailAddress = email.getText();
+
+        boolean emailIsNotUnique = registration.getAccounts().stream()
+                .anyMatch(account -> account.getEmail().equals(emailAddress));
+
+        if (emailIsNotUnique) {
+            return Reaction.failure("There is already a user with the E-Mail address '" + emailAddress + "'");
+        }
+
+        if (emailAddress.isEmpty()) {
+            return Reaction.failure("The E-Mail address can't be empty.");
+        }
+
+        Pattern emailRegex = Pattern.compile(
+                "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\" +
+                        "x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[" +
+                        "a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-" +
+                        "4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]" +
+                        ")|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x" +
+                        "01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        );
+
+        if (!emailRegex.matcher(emailAddress).matches()) {
+            return Reaction.failure("Please Provide a valid E-Mail address");
+        }
+
+        return Reaction.success();
+    }
+
+    private Reaction validateUsername() {
+        boolean usernameIsNotUnique = registration.getAccounts().stream()
+                .anyMatch(account -> account.getUsername().equals(username.getText()));
+
+        if (usernameIsNotUnique) {
+            return Reaction.failure("There is already a user called '" + username.getText() + "'");
+        }
+
+        if (username.getText().isEmpty()) {
+            return Reaction.failure("The username can't be empty.");
+        }
+
+        return Reaction.success();
     }
 
     private Reaction validatePassword() {
