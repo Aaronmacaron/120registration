fcb3a1f1b5872b08f23f8d7c12e83837a6ce2dce
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-04-08 18:31:23 +0200

    Use caching for loading scenes

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -7,57 +7,50 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registration extends Application {
 
-    private Scene registerScene;
-    private Scene loginScene;
-    private Scene accountsScene;
+    private Map<Class, Scene> sceneCache = new HashMap<>();
     private Stage primaryStage;
     private ObservableList<Account> accounts = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
+        primaryStage.setTitle(getClass().getName());
         primaryStage.setScene(getRegisterScene());
         primaryStage.show();
     }
 
     public Scene getRegisterScene() {
-        if (this.registerScene != null) {
-            return this.registerScene;
-        }
         return getScene(Register.class);
     }
 
     public Scene getLoginScene() {
-        if (this.loginScene != null) {
-            return this.loginScene;
-        }
         return getScene(Login.class);
     }
 
     public Scene getAccountsScene() {
-        if (this.accountsScene != null) {
-            return this.accountsScene;
-        }
         return getScene(Accounts.class);
     }
 
     public Scene getScene(Class clazz) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(clazz.getName() + ".fxml"));
-        Parent parent  = null;
-        try {
-            parent = fxmlLoader.load();
-            RegistrationController controller = fxmlLoader.getController();
-            controller.setRegistration(this);
-            controller.loaded();
-            return new Scene(parent);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        throw new RuntimeException("FATAL ERROR!");
+        return sceneCache.computeIfAbsent(clazz, cl -> {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(cl.getName() + ".fxml"));
+            Parent parent;
+            try {
+                parent = fxmlLoader.load();
+                RegistrationController controller = fxmlLoader.getController();
+                controller.setRegistration(this);
+                controller.loaded();
+                return new Scene(parent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            throw new RuntimeException("FATAL ERROR!");
+        });
     }
 
     public Stage getPrimaryStage() {
